c4808ae0cd1afeb0231ef843138939deab9879fa
Kelvin Liang <kelvin9877@gmail.com>
2020-02-17 20:15:20 +0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(pros) {
+    super(pros);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 class="text-center">Starter Template</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
