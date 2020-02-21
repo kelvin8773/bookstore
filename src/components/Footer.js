cd062c06f2cb82cd76886a48afbcc2c605ff9608
Kelvin Liang <kelvin9877@gmail.com>
2020-02-21 11:46:36 +0800

    linter fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => (
   <div id="app-footer">
     <div className="foot-note">
-      Handcraft by <a href="https://kelvinliang.cn/">Kelvin Liang</a> @2020.
+      Handcraft by
+      {' '}
+      <a href="https://kelvinliang.cn/">Kelvin Liang</a>
+      {' '}
+@2020.
     </div>
     <div className="foot-links">
       <a href="https://github.com/kelvin8773/bookstore/blob/master/LICENSE">LICENSE</a>
